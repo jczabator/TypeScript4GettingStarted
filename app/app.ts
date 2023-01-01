cbe17c214b99304ae4c4be3524c8b92aa469ec75
Jakub Czabator <jczabator@gmail.com>
2023-01-01 17:02:39 +0100

    add startGame function

---
+++ app/app.ts
@@ -0,0 +1,5 @@
+function startGame() {
+  // starting a new game
+  var messagesElement = document.getElementById("messeges");
+  messagesElement.innerText = "Welcome to MultiMath! Starting new game...";
+}
