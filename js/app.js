20362dac4d57f80aeb34e0d521f2e9d201b6cbb1
Jakub Czabator <jczabator@gmail.com>
2023-01-03 00:38:34 +0100

    5.11

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
+var Player = (function () {
+    function Player() {
+    }
+    Player.prototype.formatName = function () {
+        return this.name.toUpperCase();
+    };
+    return Player;
+}());
 function startGame() {
     var playerName = getInputValue("playername");
     logPlayer(playerName);
     postScore(80, playerName);
+    postScore(-5, playerName);
 }
 function logPlayer(name) {
     if (name === void 0) { name = "MultiMath Player"; }
@@ -18,10 +27,23 @@ function getInputValue(elementID) {
 }
 function postScore(score, playerName) {
     if (playerName === void 0) { playerName = "MultiMath Player"; }
+    var logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
     var scoreElement = document.getElementById("postedScores");
     scoreElement.innerText = "".concat(score, " - ").concat(playerName);
+    logger("Score: ".concat(score));
 }
 document.getElementById("startGame").addEventListener("click", startGame);
 var logMessage = function (message) { return console.log(message); };
-logMessage("Welcome to MultiMath!");
+function logError(err) {
+    console.error(err);
+}
+var firstPlayer = new Player();
+firstPlayer.name = "Lanier";
+console.log(firstPlayer.formatName());
 //# sourceMappingURL=app.js.map
